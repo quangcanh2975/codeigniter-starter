e4ea41f451f2bd556242c09a41be8e80aef0f6b3
quangcanh2975 <ubuntultc@gmail.com>
2021-06-07 23:08:15 +0700

    Add login controller & login view; Update .htaccess to run on Ubuntu

---
+++ application/controllers/Login.php
@@ -0,0 +1,11 @@
+<?php
+
+class Login extends CI_Controller
+{
+	public function load_form(){
+		// pass data through view
+		// $data = array('title'=>'test', 'message'=> 'Test pass data');
+		// $this->load->view('login_view', $data);
+		$this->load->view('login_view');
+	}
+}
